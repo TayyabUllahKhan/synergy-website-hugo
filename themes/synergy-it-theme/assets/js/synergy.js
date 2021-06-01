b5396ad0a972499242d473f7d2aa9743eb2bf710
Tayyab Ullah Khan <tayyab.khan@synergy-it.com>
2021-06-01 14:46:24 +0500

    Config file updated and cms imported

--- themes/synergy-it-theme/assets/js/synergy.js
+++ themes/synergy-it-theme/assets/js/synergy.js
@@ -29,3 +29,10 @@ $(document).ready(function() {
 		$('#menu-items').slideToggle();
 	});
 });
+
+
+import CMS from 'netlify-cms-app'
+// Initialize the CMS object
+CMS.init()
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate)
